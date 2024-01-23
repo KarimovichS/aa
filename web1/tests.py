4268345f140ff86e396ccd6ef8843db323d9ccc2
KarimovichS <sardorshukurov2003s@gmail.com>
2024-01-23 21:21:42 +0500

    s

--- web1/tests.py
+++ web1/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+
+class TestHelloWord(TestCase):
+    def setUp(self) -> None:
+        self.url = reverse('hello_word')
+
+    def test_gets(self):
+        response = self.client.get(self.url)
+
+        assert response.status_code == 200
+        assert response.json()['message'] == 'hello_word'
