fe6548a456c4ad38e4e28b01f428e90923a4f70b
KarimovichS <sardorshukurov2003s@gmail.com>
2024-01-25 19:15:20 +0500

    q4

--- stady1/settings/local.py
+++ stady1/settings/local.py
@@ -4,13 +4,24 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": config("SQL_ENGINE", "django.db.backends.sqlite3"),
+#         "NAME": config("SQL_DATABASE", BASE_DIR / "db.sitqle3"),
+#         "USER": config("SQL_USER", "user"),
+#         "PASSWORD": config("SQL_PASSWORD", "password"),
+#         "HOST": config("SQL_HOST", "localhost"),
+#         "PORT": config("SQL_PORT", "5432"),
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": config("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": config("SQL_DATABASE", BASE_DIR / "db.sitqle3"),
-        "USER": config("SQL_USER", "user"),
-        "PASSWORD": config("SQL_PASSWORD", "password"),
-        "HOST": config("SQL_HOST", "localhost"),
-        "PORT": config("SQL_PORT", "5432"),
+        "ENGINE": config("SQL_ENGINE"),
+        "NAME": config("SQL_DATABASE"),
+        "USER": config("SQL_USER"),
+        "PASSWORD": config("SQL_PASSWORD"),
+        "HOST": config("SQL_HOST"),
+        "PORT": config("SQL_PORT"),
     }
 }
